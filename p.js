9089f365fa7a6ecc0398ee4c5884df4681feb1d0
imran <amranmia812@gmail.com>
2023-02-25 22:59:38 +0600

    fist

--- p.js
+++ p.js
@@ -1,15 +1,45 @@
-document.getElementById('btn').addEventListener("click",function(){
-    let emailFaild = document.getElementById("user-email");
-    let emailValue = emailFaild.value;
-    let passwordFaild = document.getElementById("user-password");
-    let passwordValue = passwordFaild.value;
-    console.log(emailValue, passwordValue)
+ let myFunction = () =>{
+    fetch("https://restcountries.com/v3.1/all")
+    .then(res => res.json())
+    .then(del => onCountries(del) )
+ }
 
-    if(emailValue == "amran@gmail.com" && passwordValue == "812@gmail.com"){
-        window.location.href="two.html"
-    }
-    else{
-        alert('in vaild user')
-    }
 
-})
+ let onCountries = (countries)=>{
+    let country = document.getElementById('countries')
+//   console.log(countries[0].capital[0],countries[0].name.common)
+
+  countries.forEach(element =>{
+    // console.log(element)
+    let imran = document.createElement("div");
+    imran.classList.add("imran")
+    
+    imran.innerHTML=`
+    <h1>name: ${element.name.common}<h1>
+     <p> city: ${element.capital[0]}<p>
+     <button onclick="clickHere('${element.cca2}')">click here</button>
+    
+    `;
+    country.appendChild(imran)
+
+  })
+ }
+ let clickHere = (code) =>{
+    let url = `https://restcountries.com/v3.1/alpha/${code}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(del => button(del[0]))
+ } 
+
+
+ let button = (fl)=>{
+    // console.log(fl.flags)
+    let flags = document.getElementById('flags');
+    flags.innerHTML=`
+    <h1>name:${fl.name.common}<h1>
+     <img src="${fl.flags.png}">
+   
+    `
+ }
+
+ 
